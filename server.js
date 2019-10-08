dc164087a1e8df74d192874c6bcf0bdd7b261fd5
Rafał Paśko <rp.261000@gmail.com>
2019-10-08 20:27:34 +0200

    efsfsef

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+console.log('Console listening!');
+
+app.get('/*', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/dist/index.js'));
+})
